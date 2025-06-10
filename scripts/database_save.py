863cae96eb69a0d95eb431f37f951fedfc880e6e
Le <RLe@dot.nyc.gov>
2025-06-10 10:39:55 -0400

    PSQL integration

--- scripts/database_save.py
+++ scripts/database_save.py
@@ -1,7 +1,14 @@
-import sqlite3
+# import sqlite3
+# import os
+
+# def save_to_database(df):
+#     conn = sqlite3.connect(os.path.join("database", "accounting_data.sqlite"))
+#     df.to_sql("transactions", conn, if_exists="append", index=False)
+#     conn.close()
+from sqlalchemy import create_engine
 import os
 
 def save_to_database(df):
-    conn = sqlite3.connect(os.path.join("database", "accounting_data.sqlite"))
-    df.to_sql("transactions", conn, if_exists="append", index=False)
-    conn.close()
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database/accounting_data.sqlite")
+    engine = create_engine(DATABASE_URL)
+    df.to_sql("transactions", engine, if_exists="append", index=False)
